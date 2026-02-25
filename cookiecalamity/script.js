37a50031a697ed12027354223a7857871396a890
itzhakriley-blip <itzhakriley@gmail.com>
2026-02-25 18:43:51 -0500

    Refactor cookie game logic and add multipliers

--- cookiecalamity/script.js
+++ cookiecalamity/script.js
@@ -7,6 +7,7 @@ let superCookies = 0;
 let clicksAmount = cookies;
 let redCookiesAmount = 0;
 let redCookies = 0;
+let multiplier = 1;
 function countClicks() {
   if (document.querySelector(".button").onclick) {
   counter.clicks += clicksAmount;
@@ -18,9 +19,9 @@ function determineSuperCookieBonus() {
 function determineRedCookieBonus() {
   redCookies = redCookiesAmount * 50;
 }
-function shh() { counter.clicks += 1000; }
+// function shh() { counter.clicks += 100000; displayClicks();}
 function displayClicks() {
-  return document.querySelector(".counter").innerHTML = `You have clicked me ${counter.clicks} time(s)`;
+  return document.querySelector(".counter").innerHTML = `You have ${counter.clicks} click(s)`;
 };
 function buyCookie() {
   if (document.querySelector(".cookie").onclick && counter.clicks >= 50) {
@@ -32,7 +33,22 @@ function buyCookie() {
   determineClicks();
   }
   else if (document.querySelector(".cookie").onclick && counter.clicks < 50) {
-  return document.querySelector(".buyingError").innerHTML = "You don't have enough clicks!";
+  document.querySelector(".buyingError").innerHTML = "You don't have enough clicks!";
+  }
+}
+function disableCookie() {
+  if (cookies === 50) {
+  document.querySelector(".cookie").disabled = true;
+  }
+}
+function disableSuperCookie() {
+  if (superCookiesAmount === 25) {
+  document.querySelector(".superCookie").disabled = true;
+  }
+}
+function disableRedCookie() {
+  if (redCookiesAmount === 10) {
+  document.querySelector(".redCookie").disabled = true;
   }
 }
 function buySuperCookie() {
@@ -48,7 +64,7 @@ function buySuperCookie() {
   }
 }
 function determineClicks() {
-  clicksAmount = (cookies + superCookies + redCookies);
+  clicksAmount = (cookies + superCookies + redCookies) * multiplier;
 }
 function buyRedCookie() {
   if (document.querySelector(".redCookie").onclick && counter.clicks >= 25000) {
@@ -56,10 +72,7 @@ function buyRedCookie() {
   redCookiesAmount += 1;
   document.querySelector(".counter").innerHTML = `You have ${counter.clicks} click(s) left.`; 
   document.querySelector(".redCookieAmount").innerHTML = `You have ${redCookiesAmount} red cookies(s).`; 
-  determineClicks(); determineRedCookieBonus(); 
-  }
-  else if (document.querySelector(".redCookie").onclick && counter.clicks >= 25000 && redCookiesAmount === 1) {
-    alert("Congrats on getting the red cookie!");
+  determineClicks(); determineRedCookieBonus(); alert("Congrats on getting the red cookie!");
   }
   else if (document.querySelector(".redCookie").onclick && counter.clicks < 25000) {
   return document.querySelector(".buyingError").innerHTML = "You don't have enough clicks!";
@@ -87,4 +100,35 @@ function determineFutureRedCookies() {
   futureRedCookies = Math.floor((counter.clicks) / 25000);
   redCookieString = `You can afford ${futureRedCookies} red cookie(s).`
   document.querySelector(".futureDisplay").innerHTML = redCookieString;
+}
+function twoMultiplier() {
+  if (document.querySelector(".twoTimesMultiplier").onclick && counter.clicks >= 100000) {
+    counter.clicks -= 100000;
+    document.querySelector(".twoTimesMultiplier").disabled = true;
+    multiplier += 1;
+  }
+  else if (document.querySelector(".twoTimesMultiplier").onclick && counter.clicks < 100000) {
+  document.querySelector(".buyingError").innerHTML = "You don't have enough clicks!";
+  }
+}
+let futureRedCookies = 0;
+let cookieString = `You can afford ${futureCookies} cookies.`;
+let superCookieString = `You can afford ${futureSuperCookies} cookies.`;
+let redCookieString = `You can afford ${futureRedCookies} cookies.`;
+
+function determineFutureCookies() {
+  futureCookies = Math.floor((counter.clicks) / 50);
+  cookieString = `You can afford ${futureCookies} cookie(s).`
+  document.querySelector(".futureDisplay").innerHTML = cookieString;
+}
+function determineFutureSuperCookies() {
+  futureSuperCookies = Math.floor((counter.clicks) / 1000);
+  superCookieString = `You can afford ${futureSuperCookies} super cookie(s).`
+  document.querySelector(".futureDisplay").innerHTML = superCookieString;
+}
+function determineFutureRedCookies() {
+  futureRedCookies = Math.floor((counter.clicks) / 25000);
+  redCookieString = `You can afford ${futureRedCookies} red cookie(s).`
+  document.querySelector(".futureDisplay").innerHTML = redCookieString;
+
 }
